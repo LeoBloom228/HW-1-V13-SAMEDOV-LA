6ab38eb4cf8eb3acd7992e3eee2c43629a324c58
Leo_Bloom <samedov.leon@outlook.com>
2025-05-11 18:45:50 +0300

    Задание 1 в рамках ДЗ 1

--- HW1/file1.cpp
+++ HW1/file1.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
 
-int main()
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+
+//Задание 1
+
+void task1()
+
 {
 
-	return 0;
+	double a, b, result;
+
+	std::cout << "a: ";
+	std::cin >> a;
+	std::cout << "b: ";
+	std::cin >> b;
+
+	result = ((a * a) + (b * b)) / (1 - (((a * a * a) - b) / 3));
+
+	std::cout << std::fixed << std::setprecision(2);
+	std::cout << "Result: " << result << std::endl;
+
 }
+

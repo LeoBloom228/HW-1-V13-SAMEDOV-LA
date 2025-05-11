a33deeda1817cb95daf569e4243d849b53576bad
Leo_Bloom <samedov.leon@outlook.com>
2025-05-11 20:47:50 +0300

    Задание 3 в рамках ДЗ 1.

--- HW1/file1.cpp
+++ HW1/file1.cpp
@@ -28,23 +28,58 @@
 
 //Exercise 2
 
+//#include <iostream>
+//#include <cmath>
+//#include <cstdio>
+//#include <iomanip>
+
+//const double PI = 3.14;
+
+//int main ()
+//{
+	//double l;
+
+	//std::cout << "l: ";
+	//std::cin >> l;
+
+	//double r = l / (2 * PI);
+
+		//std::cout << "Radius of a circle is: " << r << std::endl;
+
+		//return 0;
+//}
+
+//Exercise 3
+
 #include <iostream>
 #include <cmath>
 #include <cstdio>
 #include <iomanip>
 
-const double PI = 3.14;
+int main()
 
-int main ()
 {
-	double l;
 
-	std::cout << "l: ";
-	std::cin >> l;
+	int number;
 
-	double r = l / (2 * PI);
+	std::cout << "Please, enter a number: ";
+	std::cin >> number;
 
-		std::cout << "Radius of a circle is: " << r << std::endl;
+	bool isThreedigit = (number >= 100 && number <= 999);
+	if (!isThreedigit)
+	{
+		std::cout << "Error. Please, enter a three-digit number! " << std::endl;
+		return 1;
+	}
+	
+	int first_digit = number / 100;
+	int last_digit = number % 10;
 
-		return 0;
+	std::cout << ((first_digit > last_digit) ? "The first digit is higher that the last digit." :
+		(first_digit < last_digit) ? "The last digit is higher that the first digit." :
+		"The first digit and the last digit are equal.") << std::endl;
+
+
+	return 0;
+
 }

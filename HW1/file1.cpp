15f9b2ab54bf7cba697258d64f004749841b2a13
Leo_Bloom <samedov.leon@outlook.com>
2025-05-12 23:12:14 +0300

    Добавил вспомогательные функции. Все текстовые записи в коде сделал латинскими буквами.

--- HW1/file1.cpp
+++ HW1/file1.cpp
@@ -1,62 +1,51 @@
 
-//#include <iostream>
-//#include <cmath>
-//#include <cstdio>
-//#include <iomanip>
-
-//Задание 1
-
-//int main()
-
-//{
-
-	//double a, b, result;
-
-	//std::cout << "a: ";
-	//std::cin >> a;
-	//std::cout << "b: ";
-	//std::cin >> b;
-
-	//result = ((a * a) + (b * b)) / (1 - (((a * a * a) - b) / 3));
-
-	//std::cout << std::fixed << std::setprecision(2);
-	//std::cout << "Result: " << result << std::endl;
-
-	//return 0;
-
-//}
-
-//Exercise 2
-
-//#include <iostream>
-//#include <cmath>
-//#include <cstdio>
-//#include <iomanip>
-
-//const double PI = 3.14;
-
-//int main ()
-//{
-	//double l;
-
-	//std::cout << "l: ";
-	//std::cin >> l;
-
-	//double r = l / (2 * PI);
-
-		//std::cout << "Radius of a circle is: " << r << std::endl;
-
-		//return 0;
-//}
-
-//Exercise 3
-
 #include <iostream>
 #include <cmath>
 #include <cstdio>
 #include <iomanip>
 
-int main()
+//Exercise 1
+
+void task1 ()
+
+{
+
+	double a, b, result;
+
+	std::cout << "a: ";
+	std::cin >> a;
+	std::cout << "b: ";
+	std::cin >> b;
+
+	result = ((a * a) + (b * b)) / (1 - (((a * a * a) - b) / 3));
+
+	std::cout << std::fixed << std::setprecision(2);
+	std::cout << "Result: " << result << std::endl;
+
+}
+
+//Exercise 2
+
+void task2 ()
+{
+const double PI = 3.14;
+
+
+	double l;
+
+	std::cout << "l: ";
+	std::cin >> l;
+
+	double r = l / (2 * PI);
+
+		std::cout << "Radius of a circle is: " << r << std::endl;
+
+}
+
+//Exercise 3
+
+
+void task3()
 
 {
 
@@ -69,7 +58,7 @@ int main()
 	if (!isThreedigit)
 	{
 		std::cout << "Error. Please, enter a three-digit number! " << std::endl;
-		return 1;
+		return;
 	}
 	
 	int first_digit = number / 100;
@@ -79,6 +68,15 @@ int main()
 		(first_digit < last_digit) ? "The last digit is higher that the first digit." :
 		"The first digit and the last digit are equal.") << std::endl;
 
+}
+
+int main()
+
+{
+
+	task1();
+	task2();
+	task3();
 
 	return 0;
 
